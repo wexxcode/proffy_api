018703f835fbd77c8587133d61b1c6d8795e41e7
Wesley S <wexxcode@gmail.com>
2020-08-06 12:03:05 -0300

    Criando tabelas do Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Iniciando Projeto")
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/', (request, response) => {
+    console.log(request.query);
+
+    return response.json({ message: 'Hello Word'});
+});
+
+app.listen(3000);
